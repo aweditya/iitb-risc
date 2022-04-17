fdb657006060d9b7269b27d8be73041d6b99644f
Aziz-Shameem <81754147+Aziz-Shameem@users.noreply.github.com>
2022-04-17 10:45:21 +0530

    made changes

--- 1bitShifter.vhd
+++ 1bitShifter.vhd
@@ -1,13 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity sign_extender_6bit is port(
+entity 1bitshifter is port(
 	data_in: in std_logic_vector(0 to 15); 
 	data_out: out std_logic_vector(0 to 15)
 );
-end entity sign_extender_6bit;
+end entity 1bitshifter;
 
-architecture shift of sign_extender_6bit is
+architecture shift of 1bitshifter is
 begin
 	data_out(1 to 15) <= data_in(0 to 14);
 	data_out(0) <= '0';
