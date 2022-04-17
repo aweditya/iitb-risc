16b1cefc8aeb5e2c3ebcfe732b51bd5fabdaaec4
Aziz-Shameem <81754147+Aziz-Shameem@users.noreply.github.com>
2022-04-17 10:47:37 +0530

    Fixed errors

--- 1bitShifter.vhd
+++ 1bitShifter.vhd
@@ -1,13 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity 1bitshifter is port(
+entity Onebitshifter is port(
 	data_in: in std_logic_vector(0 to 15); 
 	data_out: out std_logic_vector(0 to 15)
 );
-end entity 1bitshifter;
+end entity Onebitshifter;
 
-architecture shift of 1bitshifter is
+architecture shift of Onebitshifter is
 begin
 	data_out(1 to 15) <= data_in(0 to 14);
 	data_out(0) <= '0';
